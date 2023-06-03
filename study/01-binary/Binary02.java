54eaa1fcc39fb69db12b832b40e0b352918ae700
mayfool <691573424@qq.com>
2023-06-03 20:47:31 +0800

    [study] 位运算除法

--- study/01-binary/Binary02.java
+++ study/01-binary/Binary02.java
@@ -36,8 +36,8 @@ public class Binary02 {
 
     /**
      * 位运算实现乘法，a对于b中所有存在1的位进行左移，并对结果相加
-     * 0110
-     * X0111
+     * .   0110
+     * X   0111
      * =   0110
      * +  01100
      * + 011000
@@ -56,6 +56,42 @@ public class Binary02 {
         return ans;
     }
 
+    /**
+     * 判断是否是负数
+     */
+    public static boolean isNeg(int a) {
+        return a < 0;
+    }
+
+    /**
+     * <p>位运算实现除法</p>
+     * <p>a / b算法表达如下：将b左移到恰好小于a的值，左移的位数即结果中1的位置，再将a-b<<i，循环直到a<b</p>
+     * <p>具体的做法中，使用a右移来判断b需要左移的位数，否则b可能出现溢出，导致异常</p>
+     * <p>    101</p>
+     * <p>   ----</p>
+     * <p>11|1111</p>
+     * <p>   11</p>
+     * <p>-------</p>
+     * <p>     11</p>
+     * <p>     11</p>
+     * <p>_______</p>
+     * <p>      0</p>
+     */
+    public static int divide(int a, int b) {
+        // 负数处理有bug，先全部转换成正数
+        int x = isNeg(a) ? neg(a) : a;
+        int y = isNeg(b) ? neg(b) : b;
+        int ans = 0;
+        for (int i = 30; i >= 0; i = minus(i, 1)) {
+            //
+            if ((x >> i) >= y) {
+                ans |= 1 << i;
+                x = minus(x, y << i);
+            }
+        }
+        // 如果符号不同，返回负数
+        return isNeg(a) ^ isNeg(b) ? neg(ans) : ans;
+    }
 
     public static void main(String[] args) {
         System.out.println(add(4, 5));
@@ -63,5 +99,6 @@ public class Binary02 {
         System.out.println(minus(5, 4));
         System.out.println(multiply(5, 4));
         System.out.println(multiply(5, -4));
+        System.out.println(divide(12, 3));
     }
 }
