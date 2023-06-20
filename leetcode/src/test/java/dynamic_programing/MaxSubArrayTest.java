8057e243413979c3493262e9c8e3df3ab8dd2ab9
xuzhou <691573424@qq.com>
2023-06-20 10:10:35 +0800

    [leetcode] 剑指 Offer 11. 旋转数组的最小数字

--- leetcode/src/test/java/dynamic_programing/MaxSubArrayTest.java
+++ leetcode/src/test/java/dynamic_programing/MaxSubArrayTest.java
@@ -1,44 +1,45 @@
 package dynamic_programing;
 
 import java.util.Arrays;
+
 import org.junit.Test;
 import utils.RandomArray;
 
 /**
  * 53. 最大子序和
  * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
  * 输出：6
  * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
  * 示例 2：
- *
+ * <p>
  * 输入：nums = [1]
  * 输出：1
  * 示例 3：
- *
+ * <p>
  * 输入：nums = [0]
  * 输出：0
  * 示例 4：
- *
+ * <p>
  * 输入：nums = [-1]
  * 输出：-1
  * 示例 5：
- *
+ * <p>
  * 输入：nums = [-100000]
  * 输出：-100000
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * 1 <= nums.length <= 3 * 10^4
  * -10^5 <= nums[i] <= 10^5
- *
+ * <p>
  * 来源：力扣（LeetCode）
- * 链接：<a href="https://leetcode-cn.com/problems/maximum-subarray">...</a>
+ * 链接：<a href="https://leetcode-cn.com/problems/maximum-subarray">53. 最大子数组和</a>
  */
 public class MaxSubArrayTest {
 
---
+++ leetcode/src/test/java/search/FindMinInRotatedSortedArrayTest.java
@@ -0,0 +1,44 @@
+package search;
+
+/**
+ * <a href="https://leetcode.cn/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/?envType=study-plan-v2&envId=coding-interviews">剑指 Offer 11. 旋转数组的最小数字</a>
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ * <p>
+ * 给你一个可能存在 重复 元素值的数组 numbers ，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。请返回旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为 1。
+ * <p>
+ * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：numbers = [3,4,5,1,2]
+ * 输出：1
+ * 示例 2：
+ * <p>
+ * 输入：numbers = [2,2,2,0,1]
+ * 输出：0
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * n == numbers.length
+ * 1 <= n <= 5000
+ * -5000 <= numbers[i] <= 5000
+ * numbers 原来是一个升序排序的数组，并进行了 1 至 n 次旋转
+ * 注意：本题与主站 154 题相同：<a href="https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/">154. 寻找旋转排序数组中的最小值 II</a>
+ *
+ * @author xuzhou
+ * @since 2023/6/20 9:50
+ */
+public class FindMinInRotatedSortedArrayTest {
+
+    public int findMin(int[] nums) {
+        // 暴力方法
+        int min = Integer.MAX_VALUE;
+        for (int x : nums) {
+            min = Math.min(x, min);
+        }
+        return min;
+    }
+}
