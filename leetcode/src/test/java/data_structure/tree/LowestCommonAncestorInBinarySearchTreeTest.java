705057cc7c55d69371487c0373e14a07f79763f7
xuzhou <691573424@qq.com>
2023-07-13 11:17:56 +0800

    [leetcode] 235. 二叉搜索树的最近公共祖先

--- leetcode/src/test/java/data_structure/tree/LowestCommonAncestorInBinarySearchTreeTest.java
+++ leetcode/src/test/java/data_structure/tree/LowestCommonAncestorInBinarySearchTreeTest.java
@@ -3,6 +3,8 @@ package data_structure.tree;
 import utils.TreeNode;
 
 /**
+ * <a href="https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/">235. 二叉搜索树的最近公共祖先</a>
+ * <a href="https://leetcode.cn/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/?envType=study-plan-v2&envId=coding-interviews">剑指 Offer 68 - I. 二叉搜索树的最近公共祖先</a>
  * <p>给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
  * <p>百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，
  * 满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
@@ -26,7 +28,7 @@ import utils.TreeNode;
  * @author zhou.xu
  * @since 2020/10/9 15:52
  */
-public class LowestCommonAncestor {
+public class LowestCommonAncestorInBinarySearchTreeTest {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode ancestor = root;
