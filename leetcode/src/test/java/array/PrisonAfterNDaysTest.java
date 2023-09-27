5e4d8f9705e821553c140c726bf18838c01a0c3d
xuzhou <691573424@qq.com>
2023-09-27 15:49:23 +0800

    [leetcode] 957. N 天后的牢房

--- leetcode/src/test/java/array/PrisonAfterNDaysTest.java
+++ leetcode/src/test/java/array/PrisonAfterNDaysTest.java
@@ -1,42 +1,56 @@
 package array;
 
 import java.util.Arrays;
+
 import org.junit.Test;
 
 /**
- * N 天后的牢房
- * <p>
- * 8 间牢房排成一排，每间牢房不是有人住就是空着。
- * <p>
- * 每天，无论牢房是被占用或空置，都会根据以下规则进行更改：
- * <p>
- * 如果一间牢房的两个相邻的房间都被占用或都是空的，那么该牢房就会被占用。 否则，它就会被空置。
- * <p>
- * （请注意，由于监狱中的牢房排成一行，所以行中的第一个和最后一个房间无法有两个相邻的房间。）
- * <p>
- * 我们用以下方式描述监狱的当前状态：如果第 i 间牢房被占用，则 cell[i]==1，否则 cell[i]==0。
- * <p>
- * 根据监狱的初始状态，在 N 天后返回监狱的状况（和上述 N 种变化）。
- * <p>
+ * <a href="https://leetcode.cn/problems/prison-cells-after-n-days/">957. N 天后的牢房</a>
  *
  * @author : zhou.xu
  * @since : 2020/2/19 19:22
  */
 public class PrisonAfterNDaysTest {
 
-    public int[] prisonAfterNDays(int[] cells, int N) {
-        int left, current;
-        for (int i = 1; i <= N; i++) {
-            left = cells[0];
-            cells[0] = 0;
-            for (int j = 1; j < cells.length - 1; j++) {
-                current = cells[j];
-                cells[j] = left == cells[j + 1] ? 1 : 0;
-                left = current;
+    public int[] prisonAfterNDays(int[] cells, int n) {
+        int[] map = new int[128]; // 保存牢房不同状态出现的天数
+        int[][] queue = new int[128][]; // 按顺序保存牢房状态
+        int size = 1;
+        int[] next = cells;
+        int i = 1;
+        int loopStart = 0;
+        for (; i <= n; i++) {
+            next = next(next);
+            int number = toNumber(next);
+            if ((loopStart = map[number]) > 0) {
+                break;
             }
-            cells[cells.length - 1] = 0;
+            map[number] = i;
+            queue[size++] = next;
         }
-        return cells;
+        // 第i天出现重复
+        if (i >= n) {
+            return next;
+        }
+        int leftDays = (n - i) % (i - loopStart);
+        return queue[loopStart + leftDays];
+    }
+
+    private int[] next(int[] cells) {
+        int n = cells.length;
+        int[] next = new int[n];
+        for (int i = 1; i < n - 1; i++) {
+            next[i] = ~(cells[i - 1] ^ cells[i + 1]) & 1;
+        }
+        return next;
+    }
+
+    private int toNumber(int[] cells) {
+        int ans = 0;
+        for (int i = 0; i < cells.length; i++) {
+            ans |= cells[i] << i;
+        }
+        return ans;
     }
 
     private void print(int[] cells, int n) {
@@ -46,10 +60,8 @@ public class PrisonAfterNDaysTest {
 
     @Test
     public void test() {
-        int[] cells = {0, 1, 0, 1, 1, 0, 0, 1};
-        for (int i = 0; i < 7; i++) {
-            prisonAfterNDays(cells, i);
-            print(cells, i);
-        }
+        int[] cells = {1, 0, 0, 1, 0, 0, 1, 0};
+        int[] nDays = prisonAfterNDays(cells, 1000000000);
+        print(nDays, 1000000000);
     }
 }
