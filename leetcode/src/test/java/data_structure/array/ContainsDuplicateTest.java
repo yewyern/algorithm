2fd02ee7967a9ce963a82b29c8f4059f4ad5defa
xuzhou <691573424@qq.com>
2023-09-27 17:16:44 +0800

    [leetcode] 217. 存在重复元素

--- leetcode/src/test/java/data_structure/array/ContainsDuplicateTest.java
+++ leetcode/src/test/java/data_structure/array/ContainsDuplicateTest.java
@@ -1,9 +1,11 @@
 package data_structure.array;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * <a href="https://leetcode.cn/problems/contains-duplicate/">217. 存在重复元素</a>
  * <p>给定一个整数数组，判断是否存在重复元素。
  * <p>
  * <p>如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
@@ -28,8 +30,18 @@ public class ContainsDuplicateTest {
 
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>(nums.length);
-        for (int i = 0; i < nums.length; i++) {
-            if (!set.add(nums[i])) {
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate2(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
         }
