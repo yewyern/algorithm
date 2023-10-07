2253ee0d05321aaf538eb4984808144ae964ad5c
xuzhou <691573424@qq.com>
2023-10-07 13:52:06 +0800

    [leetcode] 693. 交替位二进制数

--- leetcode/src/test/java/binary/HasAlternatingBits.java
+++ leetcode/src/test/java/binary/HasAlternatingBits.java
@@ -1,8 +1,9 @@
-package easy;
+package binary;
 
 import org.junit.Test;
 
 /**
+ * <a href="https://leetcode.cn/problems/binary-number-with-alternating-bits">693. 交替位二进制数</a>
  * 给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。
  *
  * 示例 1：
