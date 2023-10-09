c39408d409a972845ab531cef5a72668cbc4c515
xuzhou <691573424@qq.com>
2023-10-09 17:06:10 +0800

    [leetcode] 82. 删除排序链表中的重复元素 II

--- leetcode/src/test/java/utils/ListNode.java
+++ leetcode/src/test/java/utils/ListNode.java
@@ -2,8 +2,10 @@ package utils;
 
 public class ListNode {
 
-    public final int val;
+    public int val;
     public ListNode next;
+
+    public ListNode() {}
 
     public ListNode(int x) {
         val = x;
