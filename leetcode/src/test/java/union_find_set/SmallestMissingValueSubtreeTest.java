51ce12c55e931952132ffe10eec3f4f3bba62a39
mayfool <691573424@qq.com>
2023-11-04 17:40:13 +0800

    [leetcode] 2003. 每棵子树内缺失的最小基因值

--- leetcode/src/test/java/union_find_set/SmallestMissingValueSubtreeTest.java
+++ leetcode/src/test/java/union_find_set/SmallestMissingValueSubtreeTest.java
@@ -2,11 +2,14 @@ package union_find_set;
 
 import org.junit.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
  * <a href="https://leetcode.cn/problems/smallest-missing-genetic-value-in-each-subtree/">2003. 每棵子树内缺失的最小基因值</a>
+ *
  * @author xuzhou
  * @since 2023/11/3 16:31
  */
@@ -14,11 +17,104 @@ public class SmallestMissingValueSubtreeTest {
 
     @Test
     public void test() {
-        int[] res = smallestMissingValueSubtree(new int[]{-1, 0, 0, 2}, new int[]{1, 2, 3, 4});
+        int[] res = smallestMissingValueSubtree(new int[]{-1, 0, 0, 0, 2}, new int[]{6, 4, 3, 2, 1});
         System.out.println(Arrays.toString(res));
     }
 
+    private int[] parents;
+    private int[] size;
+
     public int[] smallestMissingValueSubtree(int[] parents, int[] nums) {
+        int n = nums.length;
+        if (!containsOne(nums)) {
+            int[] res = new int[n];
+            Arrays.fill(res, 1);
+            return res;
+        }
+        initUnionFind(nums);
+        Node root = toTree(parents, nums);
+        int[] res = new int[n];
+        dfs(root, res);
+        return res;
+    }
+
+    private void dfs(Node node, int[] res) {
+        if (node.child.isEmpty()) {
+            res[node.index] = node.val == 1 ? 2 : 1;
+            return;
+        }
+        int max = 1;
+        for (Node child : node.child) {
+            dfs(child, res);
+            max = Math.max(max, res[child.index]);
+        }
+        for (Node child : node.child) {
+            union(child.val, node.val);
+        }
+        if (max == 1 && node.val != 1) {
+            res[node.index] = 1;
+        } else {
+            for (int i = max; i <= res.length + 1; i++) {
+                if (!isSameSet(node.val, i)) {
+                    res[node.index] = i;
+                    break;
+                }
+            }
+        }
+    }
+
+    private void initUnionFind(int[] nums) {
+        parents = new int[100002];
+        size = new int[100002];
+        for (int num : nums) {
+            parents[num] = num;
+            size[num] = 1;
+        }
+    }
+
+    private int findFather(int i) {
+        if (parents[i] != i) {
+            int father = findFather(parents[i]);
+            parents[i] = father;
+            return father;
+        }
+        return i;
+    }
+
+    private boolean isSameSet(int i, int j) {
+        if (i == j) {
+            return true;
+        }
+        if (size[i] == 0 || size[j] == 0) {
+            return false;
+        }
+        return findFather(i) == findFather(j);
+    }
+
+    private void union(int i, int j) {
+        int fi = findFather(i);
+        int fj = findFather(j);
+        if (fi != fj) {
+            if (size[fi] > size[fj]) {
+                parents[fj] = fi;
+                size[fi] += size[fj];
+            } else {
+                parents[fi] = fj;
+                size[fj] += size[fi];
+            }
+        }
+    }
+
+    private boolean containsOne(int[] nums) {
+        for (int num : nums) {
+            if (num == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Node toTree(int[] parents, int[] nums) {
         int n = nums.length;
         Node[] nodes = new Node[n];
         for (int i = 0; i < n; i++) {
@@ -27,109 +123,20 @@ public class SmallestMissingValueSubtreeTest {
         for (int i = 1; i < n; i++) {
             nodes[parents[i]].child.add(nodes[i]);
         }
-        UnionFind unionFind = new UnionFind(nodes);
-        int[] res = new int[n];
-        dfs(nodes[0], unionFind, res);
-        return res;
+        return nodes[0];
     }
 
-    private void dfs(Node node, UnionFind unionFind, int[] res) {
-        if (node.child.isEmpty()) {
-            res[node.index] = node.val == 1 ? 2 : 1;
-        }
-        int max = 1;
-        for (Node child : node.child) {
-            dfs(child, unionFind, res);
-            // todo
-            if (max == 1 || res[child.index] == 1) {
-                max = Math.max(max, res[child.index]);
-            } else {
-                max = Math.min(max, res[child.index]);
-            }
-        }
-        for (Node child : node.child) {
-            unionFind.union(child.val, node.val);
-        }
-        if (max == 1 && node.val != 1) {
-            res[node.index] = 1;
-        } else {
-            for (int i = max + 1; i <= res.length + 1; i++) {
-                if (!unionFind.isSameSet(node.val, i)) {
-                    res[node.index] = i;
-                    break;
-                }
-            }
-        }
-    }
-
-    private static class UnionFind {
-        Map<Integer, Node> nodes;
-        Map<Node, Node> parents;
-        Map<Node, Integer> size;
-
-        public UnionFind(Node[] arr) {
-            this.nodes = new HashMap<>();
-            this.parents = new HashMap<>();
-            this.size = new HashMap<>();
-            for (Node node : arr) {
-                nodes.put(node.val, node);
-                parents.put(node, node);
-                size.put(node, 1);
-            }
-        }
-
-        private Node find(Node node) {
-            if (parents.get(node) == node) {
-                return node;
-            }
-            parents.put(node, find(parents.get(node)));
-            return parents.get(node);
-        }
-
-        public boolean isSameSet(int a, int b) {
-            if (a == b) {
-                return true;
-            }
-            if (!nodes.containsKey(a) || !nodes.containsKey(b)) {
-                return false;
-            }
-            return find(nodes.get(a)) == find(nodes.get(b));
-        }
-
-        public void union(int a, int b) {
-            Node fatherA = find(nodes.get(a));
-            Node fatherB = find(nodes.get(b));
-            if (fatherA != fatherB) {
-                Integer aSize = size.get(fatherA);
-                Integer bSize = size.get(fatherB);
-                if (aSize >= bSize) {
-                    parents.put(fatherB, fatherA);
-                    size.put(fatherA, aSize + bSize);
-                    size.remove(fatherB);
-                } else {
-                    parents.put(fatherA, fatherB);
-                    size.put(fatherB, aSize + bSize);
-                    size.remove(fatherA);
-                }
-            }
-        }
-    }
-
-    private static class Node implements Comparable<Node> {
+    private static class Node {
         int index;
         int val;
         int parent;
         List<Node> child;
+
         public Node(int index, int val, int parent) {
             this.index = index;
             this.val = val;
             this.parent = parent;
             child = new ArrayList<>();
-        }
-
-        @Override
-        public int compareTo(Node other) {
-            return other.parent - this.parent;
         }
     }
 }
