1349d95b4dd0f8baacb0c64083537a6273696685
xuzhou <691573424@qq.com>
2023-11-15 14:15:22 +0800

    [leetcode] 1027. 最长等差数列

--- leetcode/src/test/java/search/FindMinInRotatedSortedArrayTest.java
+++ leetcode/src/test/java/search/FindMinInRotatedSortedArrayTest.java
@@ -2,12 +2,11 @@ package search;
 
 import org.junit.Test;
 import utils.RandomArray;
-import utils.RandomUtils;
 
 import java.util.Arrays;
 
 /**
- * <a href="https://leetcode.cn/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/?envType=study-plan-v2&envId=coding-interviews">剑指 Offer 11. 旋转数组的最小数字</a>
+ * <a href="https://leetcode.cn/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/">剑指 Offer 11. 旋转数组的最小数字</a>
  * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
  * <p>
  * 给你一个可能存在 重复 元素值的数组 numbers ，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。请返回旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为 1。
