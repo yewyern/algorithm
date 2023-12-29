3ef258f3de14ac75d54a633a1737a0efb027c2f0
xuzhou <691573424@qq.com>
2023-12-29 11:17:16 +0800

    [leetcode] 98. 验证二叉搜索树

--- leetcode/src/test/java/data_structure/tree/binarysearch/IsValidBST.java
+++ leetcode/src/test/java/data_structure/tree/binarysearch/IsValidBST.java
@@ -1,9 +1,26 @@
 package data_structure.tree.binarysearch;
 
+import utils.TreeNode;
+
 /**
+ * <a href="https://leetcode.cn/problems/validate-binary-search-tree/">98. 验证二叉搜索树</a>
  * @author xuzhou
  * @since 2023/12/28 18:24
  */
 public class IsValidBST {
 
+    TreeNode prev;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        if (prev != null && prev.val >= root.val) {
+            return false;
+        }
+        prev = root;
+        return isValidBST(root.right);
+    }
 }
