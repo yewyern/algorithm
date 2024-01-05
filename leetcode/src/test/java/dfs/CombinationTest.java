1c809cf40fd49c33b2cb84f7dfc526362c55945e
徐宙 <zhou.xu@flaginfo.com.cn>
2024-01-05 22:24:04 +0800

    [leetcode] 77. 组合

--- leetcode/src/test/java/dfs/CombinationTest.java
+++ leetcode/src/test/java/dfs/CombinationTest.java
@@ -12,6 +12,25 @@ public class CombinationTest {
 
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
+        dfs(ans, new ArrayList<>(), 1, n, k);
+        return ans;
+    }
+
+    private void dfs(List<List<Integer>> ans, List<Integer> state, int start, int n, int k) {
+        if (k == 0) {
+            ans.add(new ArrayList<>(state));
+            return;
+        }
+        int end = n - k + 1;
+        for (int i = start; i <= end; i++) {
+            state.add(i);
+            dfs(ans, state, i + 1, n, k - 1);
+            state.remove(state.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combine2(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
         if (k == 1) {
             for (int i = 1; i <= n; i++) {
                 List<Integer> a = new ArrayList<>();
