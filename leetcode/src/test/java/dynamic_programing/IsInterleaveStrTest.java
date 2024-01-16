5d85f8d0e386c612c04f47a866503354399db7f5
徐宙 <zhou.xu@flaginfo.com.cn>
2024-01-16 23:07:03 +0800

    [leetcode] 97. 交错字符串

--- leetcode/src/test/java/dynamic_programing/IsInterleaveStrTest.java
+++ leetcode/src/test/java/dynamic_programing/IsInterleaveStrTest.java
@@ -22,35 +22,25 @@ public class IsInterleaveStrTest {
         if (k != m + n) {
             return false;
         }
-        byte[][][] cache = new byte[m + 1][n + 1][k + 1];
-        return dfs(s1.toCharArray(), s2.toCharArray(), s3.toCharArray(), 0, 0, 0, cache);
+        byte[][] cache = new byte[m + 1][n + 1];
+        return dfs(s1.toCharArray(), s2.toCharArray(), s3.toCharArray(), 0, 0, cache);
     }
 
-    private boolean dfs(char[] s1, char[] s2, char[] s3, int i, int j, int k, byte[][][] cache) {
-        if (cache[i][j][k] != 0) {
-            return cache[i][j][k] == 1;
+    private boolean dfs(char[] s1, char[] s2, char[] s3, int i, int j, byte[][] cache) {
+        if (cache[i][j] != 0) {
+            return cache[i][j] == 1;
         }
         boolean ans;
-        if (k == s3.length) {
+        if (i == s1.length && j == s2.length) {
             ans = true;
-        } else if (i == s1.length) {
-            ans = equals(s2, s3, j, k);
-        } else if (j == s2.length) {
-            ans = equals(s1, s3, i, k);
         } else {
-            boolean startWithS1 = s1[i] == s3[k];
-            boolean startWithS2 = s2[j] == s3[k];
-            if (startWithS1 && startWithS2) {
-                ans = dfs(s1, s2, s3, i + 1, j, k + 1, cache) || dfs(s1, s2, s3, i, j + 1, k + 1, cache);
-            } else if (startWithS1) {
-                ans = dfs(s1, s2, s3, i + 1, j, k + 1, cache);
-            } else if (startWithS2) {
-                ans = dfs(s1, s2, s3, i, j + 1, k + 1, cache);
-            } else {
-                ans = false;
+            int k = i + j;
+            ans = i < s1.length && s1[i] == s3[k] && dfs(s1, s2, s3, i + 1, j, cache);
+            if (!ans) {
+                ans = j < s2.length && s2[j] == s3[k] && dfs(s1, s2, s3, i, j + 1, cache);
             }
         }
-        cache[i][j][k] = (byte) (ans ? 1 : -1);
+        cache[i][j] = (byte) (ans ? 1 : -1);
         return ans;
     }
 
@@ -59,38 +49,19 @@ public class IsInterleaveStrTest {
         if (s3.length() != s1.length() + s2.length()) {
             return false;
         }
-        return dfs3(s1.toCharArray(), s2.toCharArray(), s3.toCharArray(), 0, 0, 0);
+        return dfs3(s1.toCharArray(), s2.toCharArray(), s3.toCharArray(), 0, 0);
     }
 
-    private boolean dfs3(char[] s1, char[] s2, char[] s3, int i, int j, int k) {
-        if (k == s3.length) {
+    private boolean dfs3(char[] s1, char[] s2, char[] s3, int i, int j) {
+        if (i == s1.length && j == s2.length) {
             return true;
         }
-        if (i == s1.length) {
-            return equals(s2, s3, j, k);
+        int k = i + j;
+        boolean ans = i < s1.length && s1[i] == s3[k] && dfs3(s1, s2, s3, i + 1, j);
+        if (ans) {
+            return true;
         }
-        if (j == s2.length) {
-            return equals(s1, s3, i, k);
-        }
-        boolean startWithS1 = s1[i] == s3[k];
-        boolean startWithS2 = s2[j] == s3[k];
-        if (startWithS1 && startWithS2) {
-            return dfs3(s1, s2, s3, i + 1, j, k + 1) || dfs3(s1, s2, s3, i, j + 1, k + 1);
-        }
-        if (startWithS1) {
-            return dfs3(s1, s2, s3, i + 1, j, k + 1);
-        }
-        if (startWithS2) {
-            return dfs3(s1, s2, s3, i, j + 1, k + 1);
-        }
-        return false;
+        return j < s2.length && s2[j] == s3[k] && dfs3(s1, s2, s3, i, j + 1);
     }
 
-    private boolean equals(char[] a, char[] b, int i, int j) {
-        while (i < a.length && j < b.length && a[i] == b[j]) {
-            i++;
-            j++;
-        }
-        return j == b.length;
-    }
 }
