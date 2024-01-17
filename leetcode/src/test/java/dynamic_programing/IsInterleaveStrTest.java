9b82f07e14bcf6f501c12f22bac096c5166fb7fc
xuzhou <691573424@qq.com>
2024-01-17 16:01:37 +0800

    [leetcode] 97. 交错字符串

--- leetcode/src/test/java/dynamic_programing/IsInterleaveStrTest.java
+++ leetcode/src/test/java/dynamic_programing/IsInterleaveStrTest.java
@@ -15,6 +15,71 @@ public class IsInterleaveStrTest {
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
+        // 动态规划
+        return dp(s1.toCharArray(), s2.toCharArray(), s3.toCharArray());
+    }
+
+    private boolean dp(char[] s1, char[] s2, char[] s3) {
+        int m = s1.length;
+        int n = s2.length;
+        int s = s3.length;
+        if (s != m + n) {
+            return false;
+        }
+        boolean[] dp = new boolean[n + 1];
+        dp[n] = true;
+        for (int i = n - 1; i >= 0 && s2[i] == s3[i + m]; i--) {
+            dp[i] = true;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            boolean flag = s1[i] == s3[i + n];
+            dp[n] = flag;
+            for (int j = n - 1; j >= 0; j--) {
+                int k = i + j;
+                dp[j] = s1[i] == s3[k] && dp[j];
+                if (!dp[j]) {
+                    dp[j] = s2[j] == s3[k] && dp[j + 1];
+                }
+                flag = flag || dp[j];
+            }
+            if (!flag) {
+                return false;
+            }
+        }
+        return dp[0];
+    }
+
+    private boolean dp1(char[] s1, char[] s2, char[] s3) {
+        int m = s1.length;
+        int n = s2.length;
+        int s = s3.length;
+        if (s != m + n) {
+            return false;
+        }
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[m][n] = true;
+        for (int i = n - 1; i >= 0 && s2[i] == s3[i + m]; i--) {
+            dp[m][i] = true;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            boolean flag = s1[i] == s3[i + n];
+            dp[i][n] = flag;
+            for (int j = n - 1; j >= 0; j--) {
+                int k = i + j;
+                dp[i][j] = s1[i] == s3[k] && dp[i + 1][j];
+                if (!dp[i][j]) {
+                    dp[i][j] = s2[j] == s3[k] && dp[i][j + 1];
+                }
+                flag = flag || dp[i][j];
+            }
+            if (!flag) {
+                return false;
+            }
+        }
+        return dp[0][0];
+    }
+
+    public boolean isInterleave2(String s1, String s2, String s3) {
         // 记忆化搜索
         int m = s1.length();
         int n = s2.length();
