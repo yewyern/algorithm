3309773e2701cd4b61449d8d6d59d0322ed358eb
xuzhou <691573424@qq.com>
2024-01-18 16:57:55 +0800

    [leetcode] 221. 最大正方形

--- leetcode/src/test/java/dynamic_programing/MaximalSquareTest.java
+++ leetcode/src/test/java/dynamic_programing/MaximalSquareTest.java
@@ -3,6 +3,8 @@ package dynamic_programing;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * <a href='https://leetcode.cn/problems/maximal-square/'>221. 最大正方形</a>
@@ -14,46 +16,53 @@ public class MaximalSquareTest {
     @Test
     public void test() {
         System.out.println(maximalSquare(new char[][]{
-                {'0','1','1','0','1'},
-                {'1','1','0','1','0'},
-                {'0','1','1','1','0'},
-                {'1','1','1','1','0'},
-                {'1','1','1','1','1'},
-                {'0','0','0','0','0'}
+                {'1','0','1','0','0','1','1','1','0'},
+                {'1','1','1','0','0','0','0','0','1'},
+                {'0','0','1','1','0','0','0','1','1'},
+                {'0','1','1','0','0','1','0','0','1'},
+                {'1','1','0','1','1','0','0','1','0'},
+                {'0','1','1','1','1','1','1','0','1'},
+                {'1','0','1','1','1','0','0','1','0'},
+                {'1','1','1','0','1','0','0','0','1'},
+                {'0','1','1','1','1','0','0','1','0'},
+                {'1','0','0','1','1','1','0','0','0'}
         }));
     }
 
 
     public int maximalSquare(char[][] matrix) {
-        // todo
         int n = matrix[0].length;
         int[] height = new int[n];
+        LinkedList<Integer> min = new LinkedList<>();
         int max = 0;
         for (char[] row : matrix) {
             int width = 1;
-            int min = 1000;
             for (int j = 0; j < n; j++) {
                 if (row[j] == '1') {
                     height[j]++;
                 } else {
                     height[j] = 0;
                 }
-                if (height[j] <= min && height[j] <= width) {
-                    min = height[j];
-                    max = Math.max(max, min);
-                    width = 1;
-                    min = 1000;
+                // 最小队列
+                while (!min.isEmpty() && height[min.peekLast()] > height[j]) {
+                    min.pollLast();
+                }
+                min.addLast(j);
+                if (height[min.peekFirst()] <= width) {
+                    Integer i = min.pollFirst();
+                    max = Math.max(max, height[i]);
+                    width = j - i;
                 } else {
                     max = Math.max(max, width);
-                    print(height, j, width, min, max);
-                    width++;
                 }
+                width++;
             }
+            min.clear();
         }
         return max * max;
     }
 
-    private void print(int[] height, int j, int width, int min, int max) {
-        System.out.println("height = " + Arrays.toString(height) + ", j = " + j + ", width = " + width + ", min = " + min + ", max = " + max);
+    private void print(int[] height, int j, int width, int max) {
+        System.out.println("height = " + Arrays.toString(height) + ", j = " + j + ", width = " + width + ", max = " + max);
     }
 }
