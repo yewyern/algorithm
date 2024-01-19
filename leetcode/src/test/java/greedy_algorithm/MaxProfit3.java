7ca3252cf006e20bc510da01d64412ecb78fb2fb
xuzhou <691573424@qq.com>
2024-01-19 13:56:09 +0800

    [leetcode] 123. 买卖股票的最佳时机 III

--- leetcode/src/test/java/greedy_algorithm/MaxProfit3.java
+++ leetcode/src/test/java/greedy_algorithm/MaxProfit3.java
@@ -1,5 +1,7 @@
 package greedy_algorithm;
 
+import org.junit.Test;
+
 /**
  * <a href="https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iii">123. 买卖股票的最佳时机 III</a>
  * @author xuzhou
@@ -7,31 +9,27 @@ package greedy_algorithm;
  */
 public class MaxProfit3 {
 
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, sellOnceMax(prices, 0, i) + sellOnceMax(prices, i, n));
-        }
-        return max;
+    @Test
+    public void test() {
+        System.out.println(maxProfit(new int[] {3,3,5,0,0,3,1,4}));
     }
 
-    private int sellOnceMax(int[] prices, int l, int r) {
-        // todo
-        if (r <= l + 1) {
-            return 0;
+    public int maxProfit(int[] prices) {
+        // 动态规划
+        int n = prices.length;
+        int[] dp = new int[n];
+        int min = prices[0];
+        for (int i = 1; i < n; i++) {
+            min = Math.min(min, prices[i]);
+            dp[i] = Math.max(dp[i - 1], prices[i] - min);
         }
-        int ans = 0;
-        int i = l, j = l + 1;
-        while (j < r) {
-            while (j < r && prices[j] >= prices[j - 1]) {
-                j++;
-            }
-            if (j <= r) {
-                ans = Math.max(prices[j - 1] - prices[i], ans);
-            }
-            i = j++;
+        int ans = dp[n - 1];
+        int max = prices[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            max = Math.max(max, prices[i]);
+            ans = Math.max(ans, max - prices[i] + dp[i]);
         }
         return ans;
     }
+
 }
