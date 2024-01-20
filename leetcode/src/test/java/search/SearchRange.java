a8b060be29ab61fc61ff4b52969abe4a0ebf31d4
mayfool <691573424@qq.com>
2024-01-20 16:09:21 +0800

    [leetcode] 34. 在排序数组中查找元素的第一个和最后一个位置

--- leetcode/src/test/java/search/SearchRange.java
+++ leetcode/src/test/java/search/SearchRange.java
@@ -1,27 +1,27 @@
 package search;
 
-import java.util.Arrays;
 
 /**
+ * <a href="https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array">34. 在排序数组中查找元素的第一个和最后一个位置</a>
  * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
- *
+ * <p>
  * 如果数组中不存在目标值 target，返回 [-1, -1]。
- *
+ * <p>
  * 进阶：
- *
+ * <p>
  * 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
- *
- *
+ * <p>
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：nums = [5,7,7,8,8,10], target = 8
  * 输出：[3,4]
  * 示例 2：
- *
+ * <p>
  * 输入：nums = [5,7,7,8,8,10], target = 6
  * 输出：[-1,-1]
  * 示例 3：
- *
+ * <p>
  * 输入：nums = [], target = 0
  * 输出：[-1,-1]
  *
@@ -30,46 +30,33 @@ import java.util.Arrays;
  */
 public class SearchRange {
 
-    public static int[] searchRange(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
             return new int[]{-1, -1};
         }
-        int left = binarySearch(nums, target, true, 0, nums.length - 1);
-        if (left < 0) {
+        int l = binarySearch(nums, target, 0, n - 1, true);
+        if (nums[l] != target) {
             return new int[]{-1, -1};
         }
-        int right = binarySearch(nums, target, false, left, nums.length - 1);
-        return new int[]{left, right};
+        int r = binarySearch(nums, target, l + 1, n - 1, false);
+        return new int[]{l, r};
     }
 
-    private static int binarySearch(int[] nums, int target, boolean leftBound, int l, int r) {
-        int ans = -1;
-
+    private int binarySearch(int[] nums, int target, int l, int r, boolean left) {
         while (l <= r) {
-            int mid = l + ((r - l) >> 1);
-            if (nums[mid] > target || (leftBound && nums[mid] >= target)) {
-                r = mid - 1;
+            int m = (l + r) >> 1;
+            if (nums[m] < target) {
+                l = m + 1;
+            } else if (nums[m] > target) {
+                r = m - 1;
+            } else if (left) {
+                r = m - 1;
             } else {
-                l = mid + 1;
-            }
-            if (nums[mid] == target) {
-                ans = mid;
+                l = m + 1;
             }
         }
-        return ans;
-    }
-
-    public static void test(int target, int... nums) {
-        int[] range = searchRange(nums, target);
-        System.out.println("nums = " + Arrays.toString(nums) + ", target = " + target);
-        System.out.println("range = " + Arrays.toString(range));
-        System.out.println("--------------------------------");
-    }
-
-    public static void main(String[] args) {
-        test(8, 5, 7, 7, 8, 8, 10);
-        test(6, 5, 7, 7, 8, 8, 10);
-        test(0);
+        return l;
     }
 
 }
