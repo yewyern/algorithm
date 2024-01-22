bd97e3dafe08964dffea513bdd25632daff3aed7
xuzhou <691573424@qq.com>
2024-01-22 18:27:42 +0800

    [leetcode] 208. 实现 Trie (前缀树)

---
+++ leetcode/src/test/java/data_structure/trie/Trie.java
@@ -0,0 +1,57 @@
+package data_structure.trie;
+
+/**
+ * <a href="https://leetcode.cn/problems/implement-trie-prefix-tree/">208. 实现 Trie (前缀树)</a>
+ * @author xuzhou
+ * @since 2024/1/22 18:18
+ */
+public class Trie {
+
+    private Node root = new Node();
+    public Trie() {
+
+    }
+
+    public void insert(String word) {
+        Node curr = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (curr.children == null) {
+                curr.children = new Node[26];
+            }
+            if (curr.children[i] == null) {
+                curr.children[i] = new Node();
+            }
+            curr = curr.children[i];
+        }
+    }
+
+    public boolean search(String word) {
+        // todo
+        Node curr = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (curr.children == null || curr.children[i] == null) {
+                return false;
+            }
+            curr = curr.children[i];
+        }
+        return true;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node curr = root;
+        for (char c : prefix.toCharArray()) {
+            int i = c - 'a';
+            if (curr.children == null || curr.children[i] == null) {
+                return false;
+            }
+            curr = curr.children[i];
+        }
+        return true;
+    }
+
+    class Node {
+        Node[] children;
+    }
+}
