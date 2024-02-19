7a50a0cc12bf7484843ad444a4fcf9c4c1bc7eb4
mayfool <691573424@qq.com>
2024-02-19 23:02:24 +0800

    [leetcode] 117. 填充每个节点的下一个右侧节点指针 II

--- leetcode/src/test/java/data_structure/tree/MaxDepthTree.java
+++ leetcode/src/test/java/data_structure/tree/MaxDepthTree.java
@@ -1,13 +1,15 @@
 package data_structure.tree;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import utils.Pair;
 import utils.TreeNode;
 import utils.TreeUtils;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * <a href="https://leetcode.cn/problems/er-cha-shu-de-shen-du-lcof/?envType=study-plan-v2&envId=coding-interviews">剑指 Offer 55 - I. 二叉树的深度</a>
+ * <a href="https://leetcode.cn/problems/maximum-depth-of-binary-tree/">104. 二叉树的最大深度</a>
  * <p>给定一个二叉树，找出其最大深度。
  * <p>
  * <p>二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
