9f6ffce105540ac45211f871cd37d2d72e17c082
mayfool <691573424@qq.com>
2024-02-19 22:03:38 +0800

    [leetcode] 20. 有效的括号

--- leetcode/src/test/java/easy/IsValidBrackets.java
+++ leetcode/src/test/java/easy/IsValidBrackets.java
@@ -1,6 +1,11 @@
 package easy;
 
-class IsValidBrackets {
+import java.util.Stack;
+
+/**
+ * <a href="https://leetcode.cn/problems/valid-parentheses">20. 有效的括号</a>
+ */
+public class IsValidBrackets {
 
     //给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
     //
@@ -31,56 +36,6 @@ class IsValidBrackets {
     //输入: "{[]}"
     //输出: true
 
-//    public static boolean isValid(String s) {
-//        if (s == null || s.length() == 0) {
-//            return true;
-//        }
-//        if (s.length() % 2 == 1) {
-//            return false;
-//        }
-//        Stack<Character> stack = new Stack<>();
-//        for (int i = 0; i < s.length(); i++) {
-//            char c = s.charAt(i);
-//            if (c == '}' || c == ']' || c == ')') {
-//                if (stack.isEmpty() || !isPair(stack.pop(), c)) {
-//                    return false;
-//                }
-//            } else {
-//                stack.push(c);
-//            }
-//        }
-//        return stack.isEmpty();
-//    }
-
-    //    public static boolean isValid(String s) {
-//        if (s == null || s.length() % 2 == 1) {
-//            return false;
-//        }
-//        Stack<Character> stack = new Stack<>();
-//        // toCharArray遍历比charAt快
-//        for (char c : s.toCharArray()) {
-//            if (c == '}' || c == ']' || c == ')') {
-//                if (stack.isEmpty() || !isPair(stack.pop(), c)) {
-//                    return false;
-//                }
-//            } else {
-//                stack.push(c);
-//            }
-//        }
-//        return stack.isEmpty();
-//    }
-
-//    private static boolean isPair(char left, char right) {
-//        if (left == '{') {
-//            return right == '}';
-//        } else if (left == '[') {
-//            return right == ']';
-//        } else if (left == '(') {
-//            return right == ')';
-//        }
-//        return false;
-//    }
-
     public static boolean isValid(String s) {
         if (s == null || s.length() % 2 == 1) {
             return false;
@@ -109,6 +64,56 @@ class IsValidBrackets {
             }
         }
         return topIndex == 0;
+    }
+
+    public static boolean isValid1(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '}' || c == ']' || c == ')') {
+                if (stack.isEmpty() || !isPair(stack.pop(), c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isValid2(String s) {
+        if (s == null || s.length() % 2 == 1) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        // toCharArray遍历比charAt快
+        for (char c : s.toCharArray()) {
+            if (c == '}' || c == ']' || c == ')') {
+                if (stack.isEmpty() || !isPair(stack.pop(), c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private static boolean isPair(char left, char right) {
+        if (left == '{') {
+            return right == '}';
+        } else if (left == '[') {
+            return right == ']';
+        } else if (left == '(') {
+            return right == ')';
+        }
+        return false;
     }
 
     public static void main(String[] args) {
