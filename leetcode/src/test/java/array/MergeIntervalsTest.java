c97e7cca7341e121d4de6700e29096ebbd718855
xuzhou <691573424@qq.com>
2024-02-19 18:43:41 +0800

    [leetcode] 56. 合并区间

--- leetcode/src/test/java/array/MergeIntervalsTest.java
+++ leetcode/src/test/java/array/MergeIntervalsTest.java
@@ -16,6 +16,45 @@ public class MergeIntervalsTest {
             return intervals;
         }
         // 对区间按照左边界升序排序
+        int min = 10001, max = 0;
+        for (int[] a : intervals) {
+            min = Math.min(min, a[0]);
+            max = Math.max(max, a[0]);
+        }
+        int[] sorted = new int[max + 1];
+        for (int[] a : intervals) {
+            sorted[a[0]] = Math.max(sorted[a[0]], a[1]);
+        }
+
+        int size = 0;
+        int start = min;
+        int end = sorted[min];
+
+        for (int i = min + 1; i <= max; i++) {
+            if (sorted[i] == 0) {
+                continue;
+            }
+            // 如果当前区间的左边界大于上一区间的右边界，则代表它们不重叠，将上一区间添加到结果中
+            if (i > end) {
+                intervals[size++] = new int[]{start, end};
+                start = i;
+                end = sorted[i];
+            } else {
+                // 否则，它们重叠，更新右边界为两个区间右边界中的较大值
+                end = Math.max(end, sorted[i]);
+            }
+        }
+        // 不要忘记将最后一个区间添加到结果中
+        intervals[size++] = new int[]{start, end};
+        return Arrays.copyOfRange(intervals, 0, size);
+    }
+
+    public int[][] merge1(int[][] intervals) {
+        int n = intervals.length;
+        if (n < 2) {
+            return intervals;
+        }
+        // 对区间按照左边界升序排序
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
         int size = 0;
